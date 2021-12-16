adeb286376ebfc44b96d623410653b7dfa94c191
wzz7 <1059728530@qq.com>
2021-12-16 18:05:21 +0800

    v0.0.7

--- src/com/company/dao/idao/IParkingDao.java
+++ src/com/company/dao/idao/IParkingDao.java
@@ -2,11 +2,13 @@ package com.company.dao.idao;
 
 import com.company.dao.pojo.Parking;
 
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
 public interface IParkingDao {
-    List<Parking> findAllParking();
-    List<Parking> findById();
-    int updateParking();
+    List<Parking> findAllParking() throws SQLException;
+    List<Parking> findById(int id) throws SQLException;
+    List<Parking> findByCid(int cid) throws SQLException;
+    int updateParking(Parking parking) throws SQLException;
 }
--- src/com/company/dao/impl/ParkingDao.java
+++ src/com/company/dao/impl/ParkingDao.java
@@ -2,22 +2,70 @@ package com.company.dao.impl;
 
 import com.company.dao.idao.IParkingDao;
 import com.company.dao.pojo.Parking;
+import com.company.dao.util.DBUtil;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingDao implements IParkingDao {
     @Override
-    public List<Parking> findAllParking() {
-        return null;
+    public List<Parking> findAllParking() throws SQLException {
+        Connection conn = DBUtil.getConnection();
+        String sql = "SELECT b.`name`,b.address,lid,uid,end_date FROM db_parking a, db_community b WHERE a.cid=b.id;";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        List<Parking> parkingList = new ArrayList<>();
+        while (rs.next()) {
+            Parking parking = new Parking(rs.getString(1),rs.getString(2),rs.getInt(3),rs.getInt(4),rs.getDate(5));
+            parkingList.add(parking);
+        }
+        DBUtil.close(rs,ps,conn);
+        return parkingList;
     }
 
     @Override
-    public List<Parking> findById() {
-        return null;
+    public List<Parking> findById(int id) throws SQLException {
+        Connection conn = DBUtil.getConnection();
+        String sql = "SELECT b.`name`,b.address,a.lid,a.uid,a.end_date FROM db_parking a, db_community b WHERE a.cid=b.id AND a.uid = ?;";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1,id);
+        ResultSet rs = ps.executeQuery();
+        List<Parking> parkingList = new ArrayList<>();
+        while (rs.next()) {
+            Parking parking = new Parking(rs.getString(1),rs.getString(2),rs.getInt(3),rs.getInt(4),rs.getDate(5));
+            parkingList.add(parking);
+        }
+        DBUtil.close(rs,ps,conn);
+        return parkingList;
     }
 
     @Override
-    public int updateParking() {
-        return 0;
+    public List<Parking> findByCid(int cid) throws SQLException {
+        Connection conn = DBUtil.getConnection();
+        String sql = "SELECT b.`name`,b.address,a.lid,a.uid,a.end_date FROM db_parking a, db_community b WHERE a.cid=b.id AND a.cid = ?;";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1,cid);
+        ResultSet rs = ps.executeQuery();
+        List<Parking> parkingList = new ArrayList<>();
+        while (rs.next()) {
+            Parking parking = new Parking(rs.getString(1),rs.getString(2),rs.getInt(3),rs.getInt(4),rs.getDate(5));
+            parkingList.add(parking);
+        }
+        DBUtil.close(rs,ps,conn);
+        return parkingList;
+    }
+
+    @Override
+    public int updateParking(Parking parking) throws SQLException {
+        Connection con = DBUtil.getConnection();
+        String sql = "update db_parking set uid=?,end_date=? where lid=?;";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1,parking.getUid());
+        ps.setDate(2, new Date(parking.getEnd_date().getTime()));
+        ps.setInt(3,parking.getLid());
+        int flag = ps.executeUpdate();
+        DBUtil.close(null,ps,con);
+        return flag;
     }
 }
